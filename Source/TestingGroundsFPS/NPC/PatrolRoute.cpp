8776b7728f345541b9aaec0f0f48884d62bbb4ef
janforys <janforys@wp.pl>
2018-03-28 13:39:11 +0200

    Complete classes from the project

---
+++ Source/TestingGroundsFPS/NPC/PatrolRoute.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestingGroundsFPS.h"
+#include "PatrolRoute.h"
+
+
+TArray<AActor*> UPatrolRoute::GetPatrolPoints() const
+{
+	return PatrolPoints;
+}
+
+
+
+
